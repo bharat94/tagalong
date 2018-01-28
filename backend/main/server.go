2cc82c4580676aaf2b8201602982fe31705c25d1
Rohan Sapre <sapre.r@husky.neu.edu>
2018-01-28 02:11:49 -0500

    added database setup table creation

--- backend/main/server.go
+++ backend/main/server.go
@@ -13,6 +13,33 @@ type server struct {
 	db *sql.DB
 }
 
+func (s *server) dbsetup() {
+    _, err := s.db.Exec("CREATE TABLE IF NOT EXISTS users (user_id serial PRIMARY KEY, firstname varchar (50), lastname varchar (50), image varchar(50));")
+    if err != nil {
+        log.Println("[ERROR]", err)
+    } else {
+        log.Println("Users table created")
+    }
+    _, err = s.db.Exec("CREATE TABLE IF NOT EXISTS events (event_id serial PRIMARY KEY, name varchar (50), description varchar (50), image varchar(50), location varchar (50));")
+    if err != nil {
+        log.Println("[ERROR]", err)
+    } else {
+        log.Println("Events table created")
+    }
+    _, err = s.db.Exec("CREATE TABLE IF NOT EXISTS event_users (event_user_id serial PRIMARY KEY, event_id integer REFERENCES events (event_id), user_id integer REFERENCES users (user_id));")
+    if err != nil {
+        log.Println("[ERROR]", err)
+    } else {
+        log.Println("Event_users table created")
+    }
+    _, err = s.db.Exec("CREATE TABLE IF NOT EXISTS likes (like_id serial PRIMARY KEY, event_id integer REFERENCES events (event_id), first_user_id integer REFERENCES users (user_id), second_user_id integer REFERENCES users (user_id));")
+    if err != nil {
+        log.Println("[ERROR]", err)
+    } else {
+        log.Println("Likes table created")
+    }
+}
+
 func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 	// slow 5 seconds query
 	_, err := s.db.Exec("SELECT pg_sleep(5)")
