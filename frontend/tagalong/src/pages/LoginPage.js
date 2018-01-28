cdc4f91d9a9322cc0cf23ace821e469fe1f290f6
Akhila Sankar <sankar.a@husky.neu.edu>
2018-01-28 02:23:59 -0500

    Attached to login... finally

--- frontend/tagalong/src/pages/LoginPage.js
+++ frontend/tagalong/src/pages/LoginPage.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
  
 import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  ScrollView
 } from 'react-native';
  
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import Container from '../components/Container';
 import Button from '../components/Button';
+import Navigation from './SelectAction'
 
-export default class Login extends Component {
+class LoginScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
     return (
-<Container>
-    <Button 
-        styles={{button: styles.transparentButton}}
-    >
-        <View style={styles.inline}>
+      <Container>
+        <Button 
+          styles={{button: styles.transparentButton}}
+          onPress={() => navigation.navigate('SelectAction', {})}
+        >
+          <View style={styles.inline}>
             <Icon name="facebook-official" size={30} color="#3B5699" />
             <Text style={[styles.buttonBlueText, styles.buttonBigText]}>  Connect </Text> 
             <Text style={styles.buttonBlueText}>with Facebook</Text>
-        </View>
-    </Button>
-</Container>
+          </View>
+      </Button>
+    </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
 transparentButton: {
-	marginTop: 600,
+  marginTop: 600,
     borderColor: '#3B5699',
     borderWidth: 2,
     justifyContent: 'center',
@@ -51,3 +52,17 @@ inline: {
     flexDirection: 'row'
 },
 });
+
+const LoginPage = StackNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
+  SelectAction: {
+    screen: Navigation,
+    navigationOptions: {
+     headerTitle: 'Select Events',
+   }
+  }
+});
+
+export default LoginPage;
